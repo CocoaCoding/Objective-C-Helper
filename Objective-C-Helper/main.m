2e8337eba18530c1b27df62ad60082454b2520f0
Holger Hinzberg <holger@hinzberg.de>
2019-07-15 21:16:51 +0200

    Initial Commit

---
+++ Objective-C-Helper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-Helper
+//
+//  Created by Holger Hinzberg on 15.07.19.
+//  Copyright © 2019 Holger Hinzberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
